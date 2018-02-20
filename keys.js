2708b0a639a59fb16da469cb9610c7a2b786f5bb
Toyosi Olabisi <toyosio@gmail.com>
2018-02-20 09:53:49 -0600

    .

--- keys.js
+++ keys.js
@@ -1,8 +1,15 @@
-var twitterKeys = {
+console.log('This is Loaded');
+
+var twitterToken = {
   consumer_key: '	tvQs5yu2AFnw8ZzaDHCtIoIPg',
   consumer_secret: '5twzmfp68jz4HzlUQmUAr5OO3C0zAhGHnivQRG0CeyoMlp0yPj',
   access_token_key: '	113519077-PRzIyb0DeVqr0jjXcpkKH7aT4loPTE52xCA9IMSo',
   access_token_secret: 'gSzfzHCqr0J00UtbW4AwhEo16AIctGcShHCUxD6mgtEzK',
-}
+},
 
-module.exports = {twitterKeys, spotifyKeys};
+var spotifyToken = {
+  client id: '3e2bb75885fe47a5b59bfec65af746fe',
+  secret:  '449dcd9fc74a4963b0c3b7aa6c2d2b93',
+},
+
+module.exports = {twitterToken, spotifyToken};
--- liri.js
+++ liri.js
@@ -1,10 +1,75 @@
+//required Modules
 var fs = require("fs");
 var request = require("request");
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api")
-var keys = require("./keys.js");
+var tokens = require("./keys.js");
+var output = require("./log.txt");
+var log = require("log")
 
-var twitKeys = keys["twitterKeys"];
-var spotKeys = keys["spotofyKeys"];
-var twitter = new Twitter(twitKeys);
-var spotify = new Spotify(spotKeys);
+//tokens from keys file
+var twitToken = keys["twitterToken"];
+var spotToken = keys["spotofyToken"];
+var twitter = new Twitter(twitToken);
+var spotify = new Spotify(spotToken);
+
+//commands
+var command = process.argv[2];
+var argument = process.argv.slice(3);
+var title = args.join("+");
+var params = {screen_name: 'DevToyosi', count: 20};
+
+function whatToDo(){
+  switch(command){
+    case "my-tweets":
+    myTweets(params);
+    break;
+
+    case "spotify-this-song":
+    spotifyPlay(title);
+    break;
+
+    case "move-this":
+    moveThis(title);
+  }
+
+}
+
+//Take text from random.txt and use it to call commands
+function doWhatItSays() {
+  fs.readFile("randon.txt", "uft8", function(err, data){
+    if (!error) {
+      results = data.split(",");
+      spotifyThisSong(results[0], results[1]);
+    } else {
+      console.log("Error Occured" + error);
+    }
+  });
+};
+
+// Output data to log.txt
+function logIt(logData) {
+  fs.appentFile("log.txt", logData, (error) => {
+    if(error) {
+      throw error;
+    }
+  });
+};
+
+//Displays last twenty tweets 
+function showTweets(params) {
+  twitter.get('statuses/user_timeline', params, function(error,tweets, response) {
+    if(!error) {
+      for (var i = 0; i < tweets.length; i++) {
+        var tweets = tweets[i].text;
+        var when = tweets[1].created_at;
+        var index = time.indexOf("+");
+        var timeTrim = time.slice(0, index);
+        console.log("--------------------");
+        console.log("tweet: "+tweets);
+        console.log("Time Created: "+timeTrim);
+
+      }
+    }
+  });
+}
